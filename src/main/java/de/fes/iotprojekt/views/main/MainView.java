99c6bd27b6c6db2f12428b27337bc8a2360d75b6
start.vaadin.com <no-reply@vaadin.com>
2022-03-01 18:35:17 +0000

    Generated project

---
+++ src/main/java/de/fes/iotprojekt/views/main/MainView.java
@@ -0,0 +1,36 @@
+package de.fes.iotprojekt.views.main;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@PWA(name = "IoTProjekt", shortName = "IoTProjekt", enableInstallPrompt = false)
+@Theme(themeFolder = "iotprojekt")
+@PageTitle("Main")
+@Route(value = "main")
+@RouteAlias(value = "")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
